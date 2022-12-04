c1aaf1c43b3fc7823c63ea1fa8c712df85600320
alisonsavi64 <alisonsavi64@gmail.com>
2022-12-04 16:17:24 -0300

    Slider de séries parcialmente feito

--- front-end/src/components/series/seriecard.jsx
+++ front-end/src/components/series/seriecard.jsx
@@ -1,19 +1,44 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import convertImage from '../../helpers/convertbyteimg';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 function SerieCard(props) {
+
+	const settings = {
+      dots: true,
+      infinite: true,
+      autoplay: true,
+      speed: 1000,
+      slidesToShow: 4,
+      slidesToScroll: 1
+    };
 
 	return (
 
 		<>
 				
 			<h1 class="text-white py-5 px-5 text-3xl">{props.genreTitle}</h1>	
-			<div class="flex overflow-x-auto">
-				{props.serieData.map(serie => (
-						props.genreTitle === serie.serieGenre &&  <Link class="p-1" to={`/serie/${serie.serieId}`}><img class="w-3xl h-64 border-2 p-1 mx-1" src={`data:image/${serie.serieFormatImage};base64,${convertImage(serie.serieLogo.data)}`}/> </Link>
-				))}
-			</div>
+			<Slider {...settings}>
+				
+					{props.serieData.map(serie => (
+							props.genreTitle === serie.serieGenre &&  
+
+							<Link class="p-1" to={`/serie/${serie.serieId}`}> 
+
+										
+									<img class="w-full h-44 rounded-lg"src={`data:image/${serie.serieFormatImage};base64,${convertImage(serie.serieLogo.data)}`}/>
+										
+								
+							</Link>
+						))}
+				
+			</Slider>
+			
+
+			
 
 		</>
 
